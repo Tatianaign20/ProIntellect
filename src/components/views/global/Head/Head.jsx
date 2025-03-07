e8147f23ceb041bc5031b9a056e65d31a7c49702
Tania Ryabets <tatianaign20@yandex.ru>
2025-03-07 16:53:14 +0300

    v08

--- src/components/views/global/Head/Head.jsx
+++ src/components/views/global/Head/Head.jsx
@@ -1,19 +1,35 @@
 import React from "react";
-import './Head.css'
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import "./Head.css";
 
 export const Head = () => {
+  const location = useLocation();
 
-    return (
-      <>
-      <header className={"header"}>
-        <div className={"logo"}>Pro Intellect</div>
-        <nav className={"menu"}>
-          <Link to="/" className={"link"}>Главная</Link>
-          <Link  to="/examples" className={"link"}>Примеры задач</Link>
-          <Link  to="/assessment" className={"link"}> Оценить проект</Link>
+  return (
+    <>
+      <header className="header">
+        <div className="logo">Pro Intellect</div>
+        <nav className="menu">
+          <Link
+            to="/"
+            className={`link ${location.pathname === "/" ? "active-link" : ""}`}
+          >
+            Главная
+          </Link>
+          <Link
+            to="/examples"
+            className={`link ${ location.pathname === "/examples" ? "active-link" : "" }`}
+          >
+            Примеры задач
+          </Link>
+          <Link
+            to="/assessment"
+            className={`link ${ location.pathname === "/assessment" ? "active-link" : "" }`}
+          >
+            Оценить проект
+          </Link>
         </nav>
       </header>
-      </>
-    )
-  }
+    </>
+  );
+};
